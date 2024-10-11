24bd0f89004c391b27fdefb15b8933400e1cce87
Gabrielle Jonathan Juanillo <gcjuanillo@student.apc.edu.ph>
2024-10-12 01:51:34 +0800

    Add Reptiles and Fishes

--- src/main/java/com/entjava/furryfriends/FurryfriendsApplication.java
+++ src/main/java/com/entjava/furryfriends/FurryfriendsApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FurryfriendsApplication {
 
+//    Test
     public static void main(String[] args) {
         SpringApplication.run(FurryfriendsApplication.class, args);
     }
